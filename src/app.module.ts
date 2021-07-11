3ee03be95da69ec9339a1c8d1744e86168d0aeab
mromauricio <mromauricio@gmail.com>
2021-07-11 13:09:49 -0300

    Added .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,17 @@ import { UsuariosModule } from './usuarios/usuarios.module';
 import { Usuario } from './usuarios/entities/usuario.entity';
 import { Client } from './clients/entities/client.entity';
 import { ClientsModule } from './clients/clients.module';
+require('dotenv').config()
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.pg_host,
       port: 5432,
-      username: 'mauriciooliveira',
-      password: '',
-      database: 'mauriciooliveira',
+      username: process.env.pg_user,
+      password: process.env.pg_password,
+      database: process.env.pg_database,
       entities: [Usuario, Client],
       synchronize: true,
       logging: true
